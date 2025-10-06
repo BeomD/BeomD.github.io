31a68ced950fb1f5874af46644386ace5ae2baf4
BeomD <bamjin204@gmail.com>
2025-10-06 20:50:46 +0900

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://beomd.github.io,
+  url: 'https://BeomD.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
@@ -28,7 +28,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'beomd.github.io', // Usually your repo name.
+  projectName: 'BeomD.github.io', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
